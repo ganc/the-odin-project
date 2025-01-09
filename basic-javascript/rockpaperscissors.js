60b29e74a68a447b0ad73fd70788eecc2acca9f8
ganc <ganc@users.noreply.github.com>
2025-01-08 21:39:17 -0500

    Rock Paper Scissors Buttons

--- basic-javascript/rockpaperscissors.js
+++ basic-javascript/rockpaperscissors.js
@@ -16,7 +16,13 @@ let humanScore = 0;
 let computerScore = 0;
 
 console.log("Hello World!");
-playGame();
+// playGame();
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", function(e){
+    playRound(e.target.id.toUpperCase());
+  })
+})
 
 
 
@@ -36,12 +42,6 @@ function getHumanChoice() {
 
 function isValidChoice(choice) {
   return CHOICES.includes(choice);
-  // for (let i = 0; i < CHOICES.length; i++){
-  //   if (choice === CHOICES[i]) {
-  //     return true;
-  //   }
-  // }
-  // return false;
 }
 
 function XBeatsY(X, Y){
@@ -52,10 +52,13 @@ function XBeatsY(X, Y){
   }
 }
 
-function playRound() {
+function playRoundConsole(){
+  playRound(getHumanChoice());
+}
+
+function playRound(humanChoice) {
   const computerChoice = getComputerChoice();
   console.log(computerChoice);
-  const humanChoice = getHumanChoice();
   console.log(humanChoice);
   if (XBeatsY(computerChoice, humanChoice)) {
     computerScore++;
@@ -63,21 +66,64 @@ function playRound() {
   } else if (XBeatsY(humanChoice, computerChoice)){
     humanScore++;
     console.log("Human wins round")
+  } else {
+
+    console.log("Tie!");
   }
+  displayScore();
+  checkScore();
 }
   
-function playGame() {
-  for (var i = 0; i < 5; i++){
-    playRound();
-  }
-
-  console.log(`Human: ${humanScore} | Computer: ${computerScore}`);
-  if (computerScore > humanScore){
-    console.log("Computer wins!");
-  } else if (computerScore == humanScore) {
-    console.log("Tie!");
-  } else if (computerScore < humanScore) {
-    console.log("You win!")
+function displayScore(){
+  const score = document.querySelector("#score");
+  if (score != null) {
+  score.textContent = `Human: ${humanScore} | Computer: ${computerScore}`;
+  } else {
+    console.log("score was null?")
   }
 }
+
+function checkScore(){
+  if (computerScore >= 5) {
+    const score = document.querySelector("#score");
+    const winMsg = document.createElement("h3");
+    winMsg.textContent = "Computer Wins";
+    score.appendChild(winMsg);
+    resetGame();
+  } else if (humanScore >= 5) {
+    const score = document.querySelector("#score");
+    const winMsg = document.createElement("h3");
+    winMsg.textContent = "You Win!";
+    score.appendChild(winMsg);
+    resetGame();
+  }
+}
+
+function resetGame(){
+  humanScore = 0;
+  computerScore = 0;
+  const score = document.querySelector("#score");
+  const newGameMsg = document.createElement("p");
+  newGameMsg.textContent = "Press any button to start a new game."
+  score.appendChild(newGameMsg);
+}
+
+
+
+
+
+// function playGame() {
+  // for (var i = 0; i < 5; i++){
+  //   playRound();
+  // }
+
+  // console.log(`Human: ${humanScore} | Computer: ${computerScore}`);
+  // if (computerScore > humanScore){
+  //   console.log("Computer wins!");
+  // } else if (computerScore == humanScore) {
+  //   console.log("Tie!");
+  // } else if (computerScore < humanScore) {
+  //   console.log("You win!")
+  // }
+// }
 
